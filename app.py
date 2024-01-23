d6f9d2bada20286fee751ca89e4afdceb8d90d14
jagadeeswara_reddy <jagadeeswara.reddy@otocapital.in>
2024-01-23 20:01:58 +0530

    only this commit should come not the whole list of files :/

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import argparse
+import subprocess
+
 
 parser = argparse.ArgumentParser(description='A simple script with argparse')
 parser.add_argument('-a', help='Input file path')
 args = parser.parse_args()
 
 print(f"Input file: {args}")
+
+PIPE = subprocess.PIPE
+
+
+process = subprocess.Popen(["git", "log", "-m", "-1", "--name-only", '--pretty="format:"', 'master' ])
+
+stdoutput, stderror =  process.communicate()
+
+print('stdoutput', stdoutput)
+print('stderror', stderror)
